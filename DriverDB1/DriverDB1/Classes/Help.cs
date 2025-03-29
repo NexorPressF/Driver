12c85953de98450aa9adeb8265ee557a7edf407a
NexorPressF <@>
2025-03-29 11:01:10 +0300

    Добавил таблицу и кнопки без функционала

--- DriverDB1/DriverDB1/Classes/Help.cs
+++ DriverDB1/DriverDB1/Classes/Help.cs
@@ -1,8 +1,10 @@
 using Avalonia.Controls;
+using DriverDB1.Venom;
 
 namespace DriverDB1.Classes;
 
 public static class Help
 {
     public static ContentControl MainCC;
+    public static TestContext MainTC = new TestContext();
 }
--- DriverDB1/DriverDB1/Views/InspectorView.axaml.cs
+++ DriverDB1/DriverDB1/Views/InspectorView.axaml.cs
@@ -1,6 +1,10 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using DriverDB1.Classes;
+using Microsoft.EntityFrameworkCore;
 
 namespace DriverDB1.Views;
 
@@ -9,5 +13,22 @@ public partial class InspectorView : UserControl
     public InspectorView()
     {
         InitializeComponent();
+        Help.MainTC.Drivers.Load();
+        MainDG.ItemsSource = Help.MainTC.Drivers.ToList();
+    }
+
+    private void DeleteBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void RemakeBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AddBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
